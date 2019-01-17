0585b960814d49c3211da1521b8a579178265fc6
Maicale64 <42584707+Maicale64@users.noreply.github.com>
2019-01-17 15:49:39 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 
 $(function(){
 
+$(window).scroll(function(){
+var scroll = $(window).scrollTop();
+  console.log(scroll);
+});   
+  
 $('.menu-movil').on('click', function(){
 $('.navegacion-principal').slideToggle();
 });  

fd7c4b948de9dd54adfb229c0c02dffb02a97c4e
Maicale64 <42584707+Maicale64@users.noreply.github.com>
2019-01-17 15:58:04 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 
 $(function(){
 
+var windowHeight = $(window).height();
+var barraAltura  = $('.barra').innerHeight();
+  
+console.log(barraAltura);  
 $(window).scroll(function(){
 var scroll = $(window).scrollTop();
-  console.log(scroll);
+  if(scroll > windowHeight){
+  coonsole.log("ya rebasaste la altura de la pantalla");
+  }else{
+  console.log("Aun no xd");
+  }
 });   
   
 $('.menu-movil').on('click', function(){
